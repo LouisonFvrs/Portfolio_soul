337f48b0c53a99a65911e4b593a98b67d96c166a
lfeuvrais <louison.feuvrais@gmail.com>
2023-06-25 22:51:16 +0200

    animation de l'ouverture des pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 const track = document.getElementById("image-track");
+var firstImage = document.querySelector("#image-track .image:first-child");
+var secondImage = document.querySelector("#image-track .image:nth-child(2)");
+var projectsDiv = document.querySelector(".project");
+var thirdImage = document.querySelector("#image-track .image:nth-child(3)");
+var skillDiv = document.querySelector(".skills");
+var fourImage = document.querySelector("#image-track .image:nth-child(4)");
+var veilleDiv = document.querySelector(".veille");
+var fiveImage = document.querySelector("#image-track .image:nth-child(5)");
+var contactDiv = document.querySelector(".contact");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
@@ -38,8 +47,29 @@ window.ontouchmove = e => handleOnMove(e.touches[0]);
 
 var images = document.getElementsByClassName('image');
 
-Array.from(images).forEach(function(image) {
-  image.addEventListener('click', function() {
+Array.from(images).forEach(function(image, index) {
+  image.addEventListener('dblclick', function() {
+    if (index === 0) {
+        var Div = document.querySelector('.about-me');
+        Div.classList.toggle('show');
+    }
+    if (index === 1) {
+        var Div = document.querySelector('.project');
+        Div.classList.toggle('show');
+    }
+    if (index === 2) {
+        var Div = document.querySelector('.skills');
+        Div.classList.toggle('show');
+    }
+    if (index === 3) {
+        var Div = document.querySelector('.veille');
+        Div.classList.toggle('show');
+    }
+    if (index === 4) {
+        var Div = document.querySelector('.contact');
+        Div.classList.toggle('show');
+    }
+    
     var overlay = document.createElement('div');
     overlay.classList.add('animate-overlay');
     document.body.appendChild(overlay);
